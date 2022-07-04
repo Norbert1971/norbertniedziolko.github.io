5c8f88a4f3b063441d1f5cc6d01c64377bb43747
norbert niedziolko <norbert.niedziolko@gmail.com>
2022-07-04 22:12:54 +0200

    trzecie repo ok

---
+++ out/production/003repo/README.md
@@ -0,0 +1,5 @@
+norbert.niedziolko
+moje programowanie
+
+zmiana w repozytorium, pierwszy dodany plik txt
+
---
+++ zmienne.java
@@ -0,0 +1,25 @@
+public class zmienne {
+
+
+    public static void main(String[] args) {
+        String name = "Norbert";
+        int firstNumber = 1;
+        int secondNumber = 2;
+        int result =firstNumber + secondNumber;
+
+        System.out.println(name);
+        System.out.println(result);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
