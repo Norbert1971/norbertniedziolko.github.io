8304f401f1857c0ffcbe24ac85cd39c7e261dbbb
Norbert Niedziolko <norbert.niedziolko@gmail.com>
2022-07-10 20:05:36 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ Zmienne.java
@@ -0,0 +1,19 @@
+// zarezerwowane miejsce w pamięci
+// z określonym typem danych
+
+public class Zmienne {
+    public static void main(String[] args) {
+        int firstNumber ;
+        int secondNumber;
+        int result;
+        String name = "Norbert";
+    firstNumber = 1;
+     secondNumber = 2;
+     result = firstNumber + secondNumber;
+
+    name = "Robert";
+        System.out.println(name);
+
+        System.out.println(result);
+
+}}
