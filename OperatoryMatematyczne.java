2ddbdc7d0a59ef757db7b86a7598ed995943bfbc
Norbert Niedziolko <norbert.niedziolko@gmail.com>
2022-07-10 20:07:14 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ OperatoryMatematyczne.java
@@ -0,0 +1,23 @@
+public class OperatoryMatematyczne {
+
+    public static void main(String[] args) {
+        int firstNumber =4;
+        int secondNumber =6;
+        float thirdNumber =4.0F;
+
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        float division = firstNumber / secondNumber;
+        int mod = firstNumber % secondNumber;
+
+        System.out.println("Wynik dodawania "+addition);
+        System.out.println("Wynik odejmowania "+subtraction);
+        System.out.println("Wynik mnożenia  " + multiplication);
+        System.out.println("Wynik dzielenia " + division);
+        System.out.println("Wynik modulo " + mod);
+
+
+    }
+
+}
