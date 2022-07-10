c9e7d3ab1656f15066321b65eba27d2a964be3b1
Norbert Niedziolko <norbert.niedziolko@gmail.com>
2022-07-10 18:55:02 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ TypyProsteRef.java
@@ -0,0 +1,15 @@
+public class TypyProsteRef {
+
+    // zawierają tylko suche dane
+
+
+        // liczby stałoprzecinkowe
+        byte firstNumber = 127;  // 1 bajt -128 do 127
+        short secondNumber = 32689;  // 2 bajty -32768 do 32767
+        int thirdNumber = 32768999;  //4 bajty -2 147 483 648 do 2 147 483 647
+        long fourthNumber = 2148000L;  // 8 bajtów -2^63 do (2^63)-1
+
+
+    }
+
+
